00b65e0b4044c46ecc89fb77c709b08ee153d5d5
Jordan Sobovitch <jasobovitch@gmail.com>
2017-03-24 12:33:58 -0500

    updated callback url

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
 		clientID: process.env.GOOGLE_CLIENT_ID,
 		clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-		callbackURL: 'http://localhost:3000/auth/google/callback'
+		callbackURL: 'https://nitlif.herokuapp.com/auth/google/callback'
 	},
 	function (accessToken, refreshToken, profile, done) {
 		console.log('GOOGLE PROFILE: ', profile.displayName);
